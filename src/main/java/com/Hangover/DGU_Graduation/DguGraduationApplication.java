ec5b9ef6c0d3bc462cea342d7f3cee540b9ac78c
sexy-master <hyl020415@gmail.com>
2025-09-29 15:56:50 +0900

    first commit

---
+++ src/main/java/com/Hangover/DGU_Graduation/DguGraduationApplication.java
@@ -0,0 +1,13 @@
+package com.Hangover.DGU_Graduation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DguGraduationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DguGraduationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Hangover/DGU_Graduation/DguGraduationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Hangover.DGU_Graduation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DguGraduationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
