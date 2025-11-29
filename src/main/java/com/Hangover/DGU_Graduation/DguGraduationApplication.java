dd662ccb8e4808668492b02d10062fb2a082b83c
sexy-master <hyl020415@gmail.com>
2025-11-30 03:01:13 +0900

    update

--- src/main/java/com/Hangover/DGU_Graduation/DguGraduationApplication.java
+++ src/main/java/com/Hangover/DGU_Graduation/DguGraduationApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableConfigurationProperties(JwtProperties.class) // ✅ JwtProperties 값을 yml에서 바인딩
